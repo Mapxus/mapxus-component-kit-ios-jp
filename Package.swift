427692d073651a3193de6086dcfca33465033734
guochenghao <chenghaoguo@mapxus.com>
2023-12-22 00:21:18 +0800

    v6.5.0 2023-12-21 🎉 New Features 1. The Mapxus SDKs, along with AFNetworking and YYModel, have added the PrivacyInfo.xcprivacy file to comply with Apple’s review requirements. 2. Added support for integration via Swift Package Manager. 3. The Cocoapods specs has been migrated from the trunk to a private repository at the 'https://github.com/Mapxus/mapxusSpecs.git'. 4. Enhanced computation of adsorption for seamless navigation amidst buildings.

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version = "6.5.0"
+
+let package = Package(
+  name: "MapxusComponentKit",
+  platforms: [
+    .iOS(.v9),
+  ],
+  products: [
+    .library(
+      name: "MapxusComponentKit",
+      targets: ["MapxusComponentKit"]),
+  ],
+  targets: [
+    .binaryTarget(
+      name: "MapxusComponentKit",
+      url: "https://nexus3.mapxus.co.jp/repository/ios-sdk/\(version)/mapxus-component-kit-ios-jp.zip",
+      checksum: "9674ae329e70385027719345f74f8935761c0c842c6f763f1971480ba3bc3fd9")
+  ]
+)
