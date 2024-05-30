43e8c1e38567f5491f0e3cad69941537431f8087
guochenghao <chenghaoguo@mapxus.com>
2024-05-30 09:58:57 +0800

    # CHANGELOG
    ## v6.8.0 (2024-05-27)
    
    ### 🚀 Optimization
    
    * Optimized the power consumption during the process of calculating adsorption points using `MXMRouteAdsorber`.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "6.7.0"
+let version = "6.8.0"
 
 let package = Package(
   name: "MapxusComponentKit",
@@ -19,6 +19,6 @@ let package = Package(
     .binaryTarget(
       name: "MapxusComponentKit",
       url: "https://nexus3.mapxus.co.jp/repository/ios-sdk/\(version)/mapxus-component-kit-ios-jp.zip",
-      checksum: "3ade0e7c7f469543a6be3e9c87975149d2f3a5606ecf9aa4903a12191f28401c")
+      checksum: "b3097b3334d567333fdef00c4ae1c468ee358e42cc142781773109e93be8b5db")
   ]
 )
