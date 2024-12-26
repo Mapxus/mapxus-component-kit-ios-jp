e9e6eeeed64ec2088bdf51308b616bb98f2a1c67
guochenghao <chenghaoguo@mapxus.com>
2024-12-26 12:16:02 +0800

    # CHANGELOG
    ## v7.1.0 (2024-12-23)
    
    ### 📝 Changes
    
    * When a collision takes place, the waypoint markers devised for the route will avoid causing the disappearance of the icon and the text that are related to the POI.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "7.0.0"
+let version = "7.1.0"
 
 let package = Package(
   name: "MapxusComponentKit",
@@ -19,6 +19,6 @@ let package = Package(
     .binaryTarget(
       name: "MapxusComponentKit",
       url: "https://nexus3.mapxus.co.jp/repository/ios-sdk/\(version)/mapxus-component-kit-ios-jp.zip",
-      checksum: "1fd79730ba18a3c1388cc29a1c7f71c3feed521ea8a989af20418006f7febca2")
+      checksum: "a38f92dd5b0dd243b8d588d6fd0abc02effc9d910fa58143211acde2b7fe94dd")
   ]
 )
