d6e131719f242d0d34120e6c138a07e932cf7625
chenjiajie <jiajiechen@mapxus.com>
2025-09-29 16:42:54 +0800

    # CHANGELOG
    ## v8.3.0 (2025-09-23)
    
    ### 📝 Changes
    
    * Synchronous version.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "8.2.0"
+let version = "8.3.0"
 
 let package = Package(
   name: "MapxusComponentKit",
@@ -19,6 +19,6 @@ let package = Package(
     .binaryTarget(
       name: "MapxusComponentKit",
       url: "https://nexus3.mapxus.co.jp/repository/ios-sdk/\(version)/mapxus-component-kit-ios-jp.zip",
-      checksum: "afa8a8c3b369cc6e226a56ffadfa0347519338b8c7f3002c500d7bb7b0423bbe")
+      checksum: "dbf42c6a384fd13d6ce94e95d3c5391db7a02ebf181442b10407a3c8bb7129e0")
   ]
 )
